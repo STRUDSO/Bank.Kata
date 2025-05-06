4b119da34c4a68b19da69f513e04f2e6c181523d
Søren Trudsø Mahon <strudso@gmail.com>
2025-05-06 17:44:50 +0200

    A little bit of textual design

--- csharp/BankAccount.Tests/BankAccont_Should.cs
+++ csharp/BankAccount.Tests/BankAccont_Should.cs
@@ -2,18 +2,17 @@
 
 public class BankAccont_Should
 {
-/*
-Given a client makes a deposit of 1000 on 10-01-2012
-   And a deposit of 2000 on 13-01-2012
-   And a withdrawal of 500 on 14-01-2012
-   When they print their bank statement
-   Then they would see:
+    /*
+     * Design:
+     * AccountService -> Calendar Port (Stub)
+     * AccountService -> Transaction Repository (Fake)
+     * AccountService -> IBankStatementPrinter (Spy)
+     *
+     * Transfer
+     * PrintableStatement
+     * PrintableStatementLine
+     */
 
-   Date       || Amount || Balance
-   14/01/2012 || -500   || 2500
-   13/01/2012 || 2000   || 3000
-   10/01/2012 || 1000   || 1000
-*/
     [Fact]
     public void DepositTest()
     {

e16fe5eb3501b8bbeae14af59e7aa0c650874674
Søren Trudsø Mahon <strudso@gmail.com>
2025-05-06 17:32:07 +0200

    Just the test dsl

---
+++ csharp/BankAccount.Tests/BankAccont_Should.cs
@@ -0,0 +1,32 @@
+﻿namespace BankAccount.Test;
+
+public class BankAccont_Should
+{
+/*
+Given a client makes a deposit of 1000 on 10-01-2012
+   And a deposit of 2000 on 13-01-2012
+   And a withdrawal of 500 on 14-01-2012
+   When they print their bank statement
+   Then they would see:
+
+   Date       || Amount || Balance
+   14/01/2012 || -500   || 2500
+   13/01/2012 || 2000   || 3000
+   10/01/2012 || 1000   || 1000
+*/
+    [Fact]
+    public void DepositTest()
+    {
+        Deposit(of: 1000, on: "10-01-2012");
+        Deposit(of: 2000, on: "13-01-2012");
+        WithDraw(of: 5000, on: "14-01-2012");
+        PrintBankStatement();
+        ExpectedPrintedStatement("""
+                                 Date       || Amount || Balance
+                                 14/01/2012 || -500   || 2500
+                                 13/01/2012 || 2000   || 3000
+                                 10/01/2012 || 1000   || 1000
+                                 """
+        );
+    }
+}
---
+++ csharp/BankAccount/AccountService.cs
@@ -0,0 +1,8 @@
+﻿namespace BankAccount;
+
+public class AccountService
+{
+    public void Deposit(int amount) => throw new NotImplementedException();
+    public void Withdraw(int amount) => throw new NotImplementedException();
+    public void PrintStatement() => throw new NotImplementedException();
+}
